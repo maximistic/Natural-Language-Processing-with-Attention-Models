dc909867ab0662bb13b2fccb6296d1ba26dae0b7
Sri <110153672+maximistic@users.noreply.github.com>
2024-07-23 13:34:36 +0530

    Update BasicAttention.py

--- BasicAttention.py
+++ BasicAttention.py
@@ -17,3 +17,23 @@ np.random.seed(42)
 # Synthetic vectors used to test
 encoder_states = np.random.randn(input_length, hidden_size)
 decoder_state = np.random.randn(1, hidden_size)
+
+# Weights for the neural network, these are typically learned through training
+# Use these in the alignment function below as the layer weights
+layer_1 = np.random.randn(2 * hidden_size, attention_size)
+layer_2 = np.random.randn(attention_size, 1)
+
+def alignment(encoder_states, decoder_state):
+    # First, concatenate the encoder states and the decoder state.
+    inputs = np.concatenate((encoder_states, decoder_state.repeat(input_length, axis=0)), axis=1)
+    assert inputs.shape == (input_length, 2 * hidden_size)
+    
+    # Matrix multiplication of the concatenated inputs and the first layer, with tanh activation
+    activations = np.tanh(np.matmul(inputs, layer_1))
+    assert activations.shape == (input_length, attention_size)
+    
+    # Matrix multiplication of the activations with the second layer. Remember that you don't need tanh here
+    scores = np.matmul(activations, layer_2)
+    assert scores.shape == (input_length, 1)
+    
+    return scores
