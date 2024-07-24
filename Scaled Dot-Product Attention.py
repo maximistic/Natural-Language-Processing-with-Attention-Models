c2dab35f888de6aa34c4886c96304a2985b1b6d3
Sri <110153672+maximistic@users.noreply.github.com>
2024-07-24 15:04:57 +0530

    Update Scaled Dot-Product Attention.py

--- Scaled Dot-Product Attention.py
+++ Scaled Dot-Product Attention.py
@@ -40,3 +40,41 @@ def embed(tokens, embeddings):
             output[i] = embeddings[token]
             
     return output
+
+def softmax(x, axis=0):    
+    """ Calculate softmax function for an array x
+
+        axis=0 calculates softmax across rows which means each column sums to 1 
+        axis=1 calculates softmax across columns which means each row sums to 1
+    """
+    y = np.exp(x)
+    return y / np.expand_dims(np.sum(y, axis=axis), axis)
+
+def calculate_weights(queries, keys):
+    """ Calculate the weights for scaled dot-product attention"""
+    # Replace None with your code.
+    dot = np.matmul(queries, keys.T) / np.sqrt(keys.shape[1])
+    weights = softmax(dot, axis=1)
+    assert weights.sum(axis=1)[0] == 1, "Each row in weights must sum to 1"
+    return weights
+
+# Tokenize example sentences in English and French, then get their embeddings
+sentence_en = "The agreement on the European Economic Area was signed in August 1992 ."
+tokenized_en = tokenize(sentence_en, en_words)
+embedded_en = embed(tokenized_en, en_embeddings)
+
+sentence_fr = "L accord sur la zone économique européenne a été signé en août 1992 ."
+tokenized_fr = tokenize(sentence_fr, fr_words)
+embedded_fr = embed(tokenized_fr, fr_embeddings)
+
+# These weights indicate alignment between words in English and French
+alignment = calculate_weights(embedded_fr, embedded_en)
+
+# Visualize weights to check for alignment
+fig, ax = plt.subplots(figsize=(7,7))
+ax.imshow(alignment, cmap='gray')
+ax.xaxis.tick_top()
+ax.set_xticks(np.arange(alignment.shape[1]))
+ax.set_xticklabels(sentence_en.split(" "), rotation=90, size=16);
+ax.set_yticks(np.arange(alignment.shape[0]));
+ax.set_yticklabels(sentence_fr.split(" "), size=16);
