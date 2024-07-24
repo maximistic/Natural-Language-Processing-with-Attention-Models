a69bafcb25d2a9ea4bc5013d18b3fc34597039ce
Sri <110153672+maximistic@users.noreply.github.com>
2024-07-24 15:03:23 +0530

    Update Scaled Dot-Product Attention.py

--- Scaled Dot-Product Attention.py
+++ Scaled Dot-Product Attention.py
@@ -13,3 +13,30 @@ with open("./data/word2int_fr.pkl", "rb") as f:
 # Load the word embeddings
 en_embeddings = np.load("./data/embeddings_en.npz")["embeddings"]
 fr_embeddings = np.load("./data/embeddings_fr.npz")["embeddings"]
+
+# Define some helper functions
+
+def tokenize(sentence, token_mapping):
+    tokenized = []
+    
+    for word in sentence.lower().split(" "):
+        try:
+            tokenized.append(token_mapping[word])
+        except KeyError:
+            # Using -1 to indicate an unknown word
+            tokenized.append(-1)
+        
+    return tokenized
+
+
+def embed(tokens, embeddings):
+    embed_size = embeddings.shape[1]
+    
+    output = np.zeros((len(tokens), embed_size))
+    for i, token in enumerate(tokens):
+        if token == -1:
+            output[i] = np.zeros((1, embed_size))
+        else:
+            output[i] = embeddings[token]
+            
+    return output
